5d07a433de43973699fd80ab4b47a64c6137c201
tusharkumar2002 <tshrsgr@gmail.com>
2024-12-14 15:30:44 +0530

    Add project files.

---
+++ crud dot net api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud_dot_net_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ crud dot net api/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace crud_dot_net_api.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Age{ get; set; }
+        public string Salary { get; set; }
+
+    }
+}
